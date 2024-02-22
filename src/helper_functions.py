da61fa539413ec1c6710fce812106a16d6a7d164
anvy <anvy@itu.dk>
2024-02-22 13:53:11 +0100

    update helper-functions

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,6 +1,7 @@
+import os
+import shutil
+
 def remove_output_data(output_folders, remove_previous_output: bool = False):
-    import os
-    import shutil
 
     if remove_previous_output:
         for f in output_folders:
