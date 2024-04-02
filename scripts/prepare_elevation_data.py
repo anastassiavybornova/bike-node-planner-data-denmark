521f5851eb0058aaa111c491e2378045b1904931
anvy <anvy@itu.dk>
2024-04-02 14:51:47 +0200

    minor printout updates

--- scripts/prepare_elevation_data.py
+++ scripts/prepare_elevation_data.py
@@ -2,11 +2,15 @@
 exec(open("../src/helper_functions.py").read())
 
 remove_output_data(
-    ["../data/dem", "../input-for-bike-node-planner/dem"], remove_previous_output=True
+    [
+        "../data/dem", 
+        "../input-for-bike-node-planner/dem"
+    ], 
+    remove_previous_output=True
 )
 
 # %%
-print("Downloading and merging elevation data. This may take a while... ")
+print("Downloading and merging elevation data. This can take several minutes per municipality.")
 
 exec(open("../src/download_dem.py").read())
 
