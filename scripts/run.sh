84382f632afb2d58c51a7b185dd37b84c2f3f05b
anvy <anvy@itu.dk>
2024-04-11 16:18:42 +0200

    update echo msg

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,4 @@
 $1 generate-input.py
 export PROJ_LIB=$2 # point to QGIS' proj.db location
 $1 generate-elevation.py
-echo "All done! Now you can copy-paste all subfolders of '/input-for-bike-node-planner/' into the '/data/input/' folder of bike-node-planner"
+echo "All done! \n Now you can copy-paste all subfolders of '/input-for-bike-node-planner/' into the '/data/input/' folder of bike-node-planner"
