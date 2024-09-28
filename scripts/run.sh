c978e73b341e177e247ce43494cf3db3a567fb6c
anvy <anvy@itu.dk>
2024-09-28 20:08:17 +0200

    update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
-$1 generate-input.py
-export PROJ_LIB=$2 # point to QGIS' proj.db location
-$1 -W ignore generate-elevation.py
+
+### read in command-line arguments
+while [ $# -gt 0 ]; do
+    if [[ $1 == "--"* ]]; then
+        v="${1/--/}"
+        declare "$v"="$2"
+        shift
+    fi
+    shift
+done
+
+echo "Running worflow with settings:"
+echo "download_elevation: '$download_elevation'"
+echo "python path: '$python_path'"
+echo "proj.db fiona path: '$projdb_path'"
+
+$python_path generate-input.py
+
+export PROJ_LIB=$projdb_path # point to QGIS' proj.db location
+
+if [ "$download_elevation" == "1" ] ; then 
+    $python_path -W ignore generate-elevation.py
+else 
+    echo "Elevation data download was not requested. Please provide your own elevation raster."; 
+fi
+
 echo "All done! Now you can copy-paste all subfolders of '/input-for-bike-node-planner/' into the '/data/input/' folder of bike-node-planner"
