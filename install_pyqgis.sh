545b8d8fe70c311cba7d05f5949fe3a5685cd799
anvy <anvy@itu.dk>
2024-09-29 10:26:49 +0200

    -e typo

--- install_pyqgis.sh
+++ install_pyqgis.sh
@@ -15,6 +15,6 @@ echo "Installing required Python packages for QGIS"
 $python_path -m pip install momepy contextily osmnx==1.6.0
 $python_path -m pip install numpy --force-reinstall -v numpy==1.22.4
 $python_path -m pip install --upgrade shapely lxml geopandas
-$python_path -m pip install . -e
+$python_path -m pip install -e .
 
 echo "Python packages for QGIS successfully installed"
